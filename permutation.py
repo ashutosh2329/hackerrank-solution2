51deaaec2c4151cbddd1fa85ceafd51f89671b74
ashutosh2329 <sonutiwari2329@gmail.com>
2020-06-05 01:00:19 +0530

    permu

---
+++ permutation.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the permutationEquation function below.
+def permutationEquation(p):
+    lis=[]
+    for i in range(1,len(p)+1):
+        for j in range(len(p)):
+            if(i==p[j]):
+                for k in range(len(p)):
+                    if(j+1==p[k]):
+                        lis.append(k+1)
+                        break
+                break
+    return lis
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    p = list(map(int, input().rstrip().split()))
+
+    result = permutationEquation(p)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
